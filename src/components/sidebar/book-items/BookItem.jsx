9ae8cc050ea954e404b6d02a2ebd56010b3511be
francis <franzdiaz460@gmail.com>
2023-10-30 02:45:13 +0400

    style: adding basic styling to book items

--- src/components/sidebar/book-items/BookItem.jsx
+++ src/components/sidebar/book-items/BookItem.jsx
@@ -1,7 +1,11 @@
 import bookItemStyles from "./bookItem.module.css";
 
 const BookItem = ({ author, title, totalPages, currentPages, isFinished }) => {
-  return <li className={`book-item ${bookItemStyles.container}`}>{title}</li>;
+  return (
+    <li className={`book-item ${bookItemStyles.container}`}>
+      <a className={`${bookItemStyles.linkContainer}`}>{title}</a>
+    </li>
+  );
 };
 
 export default BookItem;
--- src/components/sidebar/library-panel/LibraryPanel.jsx
+++ src/components/sidebar/library-panel/LibraryPanel.jsx
@@ -5,9 +5,41 @@ const LibraryPanel = () => {
   const placeholders = [
     {
       author: "Haruki Murakami",
-      title: "Killing commendatore",
+      title: "Killing Commendatore",
       totalPages: 367,
       currentPage: 0,
+      isFinished: false,
+    },
+
+    {
+      author: "Herman Melville",
+      title: "Moby Dick",
+      totalPages: 523,
+      currentPage: 263,
+      isFinished: false,
+    },
+
+    {
+      author: "Fyodor Dostoyevsky",
+      title: "Crime and Punishment",
+      totalPages: 490,
+      currentPage: 461,
+      isFinished: false,
+    },
+
+    {
+      author: "Viktor Frankl",
+      title: "Man's Search For Meaning",
+      totalPages: 490,
+      currentPage: 461,
+      isFinished: false,
+    },
+
+    {
+      author: "Osamu Dazai",
+      title: "No Longer Human",
+      totalPages: 262,
+      currentPage: 152,
       isFinished: false,
     },
   ];
