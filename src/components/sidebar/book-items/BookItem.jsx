43ffe3e4c553ef3e61683214a4a774ab2072506f
francis <franzdiaz460@gmail.com>
2023-10-30 23:19:04 +0400

    ref: appeneded placeholder data and added more styles to elements

--- src/components/sidebar/book-items/BookItem.jsx
+++ src/components/sidebar/book-items/BookItem.jsx
@@ -4,19 +4,20 @@ const BookItem = ({ book: { author, title, totalPages, currentPage, genres, isFi
   return (
     <li className={`book-item ${bookItemStyles.container}`}>
       <a className={`${bookItemStyles.linkContainer}`}>
-        <div id="book-item-progress-container" className={`${bookItemStyles.progressContainer}`}>
-          {totalPages - currentPage / 100}
-        </div>
-        <div id="book-item-info">
-          <p>
-            {title} {author}
-          </p>
-          <span>
-            <p>
-              Pages:{totalPages} | Page: {currentPage}
-            </p>
-          </span>
-          <p>{genres}</p>
+        <div id="contents" className={`${bookItemStyles.contentsContainer}`}>
+          <div id="book-item-progress-container" className={`${bookItemStyles.progressContainer}`}>
+            {Math.round((currentPage / totalPages) * 100)}%
+          </div>
+          <div id="book-item-info" className={`${bookItemStyles.bookInfoContainer}`}>
+            <p>{title}</p>
+            <span>
+              <p>{author}</p>
+              <p>
+                Pages:{totalPages} | Page: {currentPage}
+              </p>
+            </span>
+            <p>{genres}</p>
+          </div>
         </div>
         <div>Updated 1 week ago</div>
       </a>
