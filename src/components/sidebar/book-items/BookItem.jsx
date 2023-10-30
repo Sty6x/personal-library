8cd6f64254f9d1cfe946aff5eaf388028a903686
francis <franzdiaz460@gmail.com>
2023-10-31 00:35:47 +0400

    ref: add temporary progress circle

--- src/components/sidebar/book-items/BookItem.jsx
+++ src/components/sidebar/book-items/BookItem.jsx
@@ -6,7 +6,27 @@ const BookItem = ({ book: { author, title, totalPages, currentPage, genres, isFi
       <a className={`${bookItemStyles.linkContainer}`}>
         <div id="contents" className={`${bookItemStyles.contentsContainer}`}>
           <div id="book-item-progress-container" className={`${bookItemStyles.progressContainer}`}>
-            {Math.round((currentPage / totalPages) * 100)}%
+            <svg version="1.1" width="60" height="60" xmlns="http://www.w3.org/2000/svg">
+              <ellipse
+                stroke="#DF7868"
+                strokeWidth="3"
+                cx="50%"
+                cy="50%"
+                rx="28"
+                ry="28"
+                fill="none"
+              />
+              <text
+                fontWeight="bold"
+                x="51%"
+                y="59%"
+                font-size="15"
+                text-anchor="middle"
+                fill="white"
+              >
+                {Math.round((currentPage / totalPages) * 100)}%
+              </text>
+            </svg>
           </div>
           <div id="book-item-info" className={`${bookItemStyles.bookInfoContainer}`}>
             <p>{title}</p>
