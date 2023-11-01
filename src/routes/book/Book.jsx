a692ac73497048e8861084e68134031b4f377261
francis <franzdiaz460@gmail.com>
2023-11-01 08:02:36 +0400

    style: making canvas occupy its parents width and height

--- src/routes/book/Book.jsx
+++ src/routes/book/Book.jsx
@@ -2,10 +2,8 @@ import BookStyles from "./book.module.css";
 const Book = () => {
   return (
     <div id="book" className={BookStyles.container}>
-      {/* <h1>This is a book</h1> */}
-      <canvas id="canvas">
-        <p>Nothing here</p>
-      </canvas>
+      <canvas id="canvas" className={`${BookStyles.canvas}`} />
+      {/* <p>Nothing here</p> */}
     </div>
   );
 };
