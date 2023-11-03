d3b682ddca588708aff92ed6bc8c3a2b026287f5
francis <franzdiaz460@gmail.com>
2023-11-03 12:11:24 +0400

    ref: testing out pixijs react text

--- src/routes/book/Book.jsx
+++ src/routes/book/Book.jsx
@@ -1,14 +1,16 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import BookStyles from "./book.module.css";
 import { LibraryContext } from "../app/App";
+import { Stage, Container, Text } from "@pixi/react";
+import * as PIXI from "pixi.js";
+
 const Book = () => {
+  const bookRef = useRef();
   const { library } = useContext(LibraryContext);
   const [currentBook, setCurrentBook] = useState(() => {
     const [book] = library.filter((book) => `/${book.link}` === window.location.pathname);
     return book;
   });
-
-  const canvasRef = useRef();
 
   function filterCurrentBook(arr) {
     const [book] = arr.filter((book) => `/${book.link}` === window.location.pathname);
@@ -20,15 +22,20 @@ const Book = () => {
   }, [window.location.pathname]);
 
   return (
-    <div id="book" className={BookStyles.container}>
-      <button onClick={() => {}}>Click</button>
-      <canvas
-        width={1920}
-        height={1080}
-        ref={canvasRef}
-        id="canvas"
-        className={`${BookStyles.canvas}`}
-      />
+    <div id="book" ref={bookRef} className={BookStyles.container}>
+      <Stage>
+        <Container x={400} y={330}>
+          <Text
+            eventMode="static"
+            onclick={(e) => {
+              console.log("clicked");
+            }}
+            text="Hello World"
+            anchor={{ x: 0.5, y: 0.5 }}
+            style={new PIXI.TextStyle({ fill: ["#ffffff", "#00ff99"] })}
+          />
+        </Container>
+      </Stage>
     </div>
   );
 };
