0b861e02e835bfe5e5571df4caa0f993e6daaf89
francis <franzdiaz460@gmail.com>
2023-11-08 17:30:34 +0400

    ref: make genre input component react if the sidebar is exceeding its normal width

--- src/components/sidebar/library-panel/genre-input/GenreInput.jsx
+++ src/components/sidebar/library-panel/genre-input/GenreInput.jsx
@@ -1,40 +1,55 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Genre from "./genre/Genre";
 import genreInputStyles from "./genreInput.module.css";
 
 const GenreInput = () => {
-  const genreList = ["Drama", "Psychological", "Psychological", "Nautical", "Thriller", "History"];
+  const [genreList, setGenreList] = useState([
+    // "Drama",
+    // "Psychological",
+    // "Psychological",
+    // "Nautical",
+    // "Thriller",
+    // "History",
+  ]);
+
   const currentSidebarWidth = useRef();
 
   function wrapGenresInList() {
     const newSidebarWidth = document.getElementById("side-bar");
     console.log(newSidebarWidth.offsetWidth);
-    if (newSidebarWidth.offsetWidth > currentSidebarWidth) {
+    if (newSidebarWidth.offsetWidth > currentSidebarWidth.current) {
       console.log("too much contents not wrapping");
     }
   }
+
+  useEffect(() => {
+    wrapGenresInList();
+  }, [genreList]);
 
   useEffect(() => {
     currentSidebarWidth.current = document.getElementById("side-bar").offsetWidth;
   }, []);
 
   function addGenre(e) {
-    const inputData = new FormData(e.target);
-    const inputText = Object.fromEntries(inputData);
-    console.log(inputText);
+    // const inputData = new FormData(e.target);
+    // const inputText = Object.fromEntries(inputData);
+    // console.log(inputText);
+    setGenreList(["Psychological", ...genreList]);
   }
 
-  const displayGenres = genreList.map((genre) => {
-    // return <Genre genreText={genre} />;
+  const displayGenres = genreList.map((genre, i) => {
+    return <Genre key={`${genre}${i}`} genreText={genre} />;
   });
 
   return (
     <div className={`${genreInputStyles.container}`}>
-      <form onSubmit={addGenre} id="genre-input" className={`${genreInputStyles.inputContainer}`}>
+      <div id="genre-input" className={`${genreInputStyles.inputContainer}`}>
         <label htmlFor="genre-input">Genre</label>
         <input id="genre-input" name="genreInput" />
-        <button style={{ color: "white" }}>Add</button>
-      </form>
+        <button type="button" onClick={addGenre} style={{ color: "white" }}>
+          Add
+        </button>
+      </div>
       <div className={`${genreInputStyles.genreLists}`}>{displayGenres}</div>
     </div>
   );
