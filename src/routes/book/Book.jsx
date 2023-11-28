bb7c1455f73674909dfd159672c1e84273ba853d
francis <franzdiaz460@gmail.com>
2023-11-29 01:51:15 +0400

    ref: able to handle dragging mouse inputs

--- src/routes/book/Book.jsx
+++ src/routes/book/Book.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState, useCallback } from "react";
+import { useContext, useEffect, useRef, useState, useCallback, useMemo } from "react";
 import BookStyles from "./book.module.css";
 import { LibraryContext } from "../app/App";
 import { Stage, Container, Text, Graphics } from "@pixi/react";
@@ -6,6 +6,8 @@ import * as PIXI from "pixi.js";
 
 const Book = () => {
   const bookRef = useRef();
+  let noteIsClicked = false;
+
   // const { library } = useContext(LibraryContext);
   // const [currentBook, setCurrentBook] = useState(() => {
   //   const [book] = library.filter((book) => `/${book.link}` === window.location.pathname);
@@ -20,26 +22,49 @@ const Book = () => {
   // useEffect(() => {
   //   setCurrentBook(() => filterCurrentBook(library));
   // }, [window.location.pathname]);
-  //
+
+  function handleOnMouseDrag(pe) {
+    if (!noteIsClicked) {
+      return;
+    }
+    let distanceX = pe.movementX;
+    let distanceY = pe.movementY;
+
+    pe.currentTarget.x = pe.currentTarget.x + distanceX;
+    pe.currentTarget.y = pe.currentTarget.y + distanceY;
+  }
+
+  function initMouseClicked(pe) {
+    console.log(pe.currentTarget);
+    noteIsClicked = noteIsClicked ? false : true;
+  }
+
   const draw = useCallback((g) => {
     g.beginFill(0x68a5de);
     g.drawRoundedRect(0, 0, 450, 300, 6);
-
-    // g.drawRect(0, 0, 100, 100);
   }, []);
 
   return (
     <div id="book" ref={bookRef} className={BookStyles.container}>
-      <Stage options={{ backgroundColor: 0x1a1b1d, antialias: true }}>
-        <Container x={300} y={300}>
+      <Stage
+        // onMouseMove={handleOnMouseDrag}
+        options={{ backgroundColor: 0x1a1b1d, antialias: true }}
+      >
+        <Container
+          eventMode="static"
+          onmousemove={handleOnMouseDrag}
+          onmousedown={initMouseClicked}
+          onmouseup={initMouseClicked}
+          onmouseleave={() => {
+            noteIsClicked = false;
+          }}
+          x={300}
+          y={300}
+        >
           <Graphics draw={draw} />
           <Text
             anchor={{ x: 0, y: 0 }}
             position={{ x: 20, y: 10 }}
-            eventMode="static"
-            onclick={(e) => {
-              console.log("clicked");
-            }}
             text="Word Wrap Width is set to 80px For some reason HEHEHHEAD iawdjamoiwd wdiadj"
             style={new PIXI.TextStyle({ fill: "#1a1b1d", wordWrapWidth: 400 - 30, wordWrap: true })}
           />
