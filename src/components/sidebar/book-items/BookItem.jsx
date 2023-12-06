b8b5629cc90b88284cbac8f37f64b9cad4479900
francis-lp <franzdiaz460@gmail.com>
2023-12-06 13:42:14 +0400

    ref: changed layout so that the progress circle occupies the same space vertically as the book information containers

--- src/components/sidebar/book-items/BookItem.jsx
+++ src/components/sidebar/book-items/BookItem.jsx
@@ -17,41 +17,41 @@ const BookItem = ({
   return (
     <li className={`book-item ${bookItemStyles.container}`}>
       <NavLink to={`/${link}`} className={`${bookItemStyles.linkContainer}`}>
-        <div id="contents" className={`${bookItemStyles.contentsContainer}`}>
-          <div
-            id="book-item-progress-container"
-            className={`${bookItemStyles.progressContainer}`}
+        <div
+          id="book-item-progress-container"
+          className={`${bookItemStyles.progressContainer}`}
+        >
+          <svg
+            version="1.1"
+            width="80"
+            height="80"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <svg
-              version="1.1"
-              width="60"
-              height="60"
-              xmlns="http://www.w3.org/2000/svg"
+            <ellipse
+              stroke="#DF7868"
+              strokeWidth="3"
+              cx="50%"
+              cy="50%"
+              rx="38"
+              ry="38"
+              fill="none"
+            />
+            <text
+              fontWeight="bold"
+              x="51%"
+              y="55%"
+              fontSize="15"
+              textAnchor="middle"
+              fill="white"
             >
-              <ellipse
-                stroke="#DF7868"
-                strokeWidth="3"
-                cx="50%"
-                cy="50%"
-                rx="28"
-                ry="28"
-                fill="none"
-              />
-              <text
-                fontWeight="bold"
-                x="51%"
-                y="59%"
-                fontSize="15"
-                textAnchor="middle"
-                fill="white"
-              >
-                {currentPage === 0 && totalPages === 0
-                  ? 0
-                  : Math.round((currentPage / totalPages) * 100)}
-                %
-              </text>
-            </svg>
-          </div>
+              {currentPage === 0 && totalPages === 0
+                ? 0
+                : Math.round((currentPage / totalPages) * 100)}
+              %
+            </text>
+          </svg>
+        </div>
+        <div id="contents" className={`${bookItemStyles.contentsContainer}`}>
           <div
             id="book-item-info"
             className={`${bookItemStyles.bookInfoContainer}`}
@@ -71,9 +71,10 @@ const BookItem = ({
               </p>
             </span>
           </div>
-        </div>
-        <div className={bookItemStyles.updateTime}>
-          <p>{formatDistance(new Date(), lastUpdated)} ago</p>
+
+          <div className={bookItemStyles.updateTime}>
+            <p>{formatDistance(new Date(), lastUpdated)} ago</p>
+          </div>
         </div>
       </NavLink>
     </li>
