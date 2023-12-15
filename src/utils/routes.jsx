cc71930ac7d93d319642eda880a7e923cfd4c490
francis-lp <franzdiaz460@gmail.com>
2023-12-15 20:32:47 +0400

    ref: built a simple error component to handle unrecognized endpoint

--- src/utils/routes.jsx
+++ src/utils/routes.jsx
@@ -3,12 +3,18 @@ import App from "../routes/app/App.jsx";
 import { createBrowserRouter } from "react-router-dom";
 import { getLocalStorage } from "./localStorage.js";
 import StartingScreen from "../routes/StartingScreen.jsx";
+import ErrorPage from "../components/error/ErrorPage.jsx";
 
 export const ROUTES = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     loader: getLocalStorage,
+    errorElement: (
+      <ErrorPage
+        message={"The book that you're trying to access does not exist."}
+      />
+    ),
     children: [
       {
         path: "/:bookId",
