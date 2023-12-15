7b99bf2e2378ad2523a148f68cbabbb6e4d5d85d
francis-lp <franzdiaz460@gmail.com>
2023-12-15 17:06:29 +0400

    ref: add arrow for zooming

--- src/components/book-components/Arrows.jsx
+++ src/components/book-components/Arrows.jsx
@@ -43,6 +43,47 @@ const Arrows = () => {
     }
   };
 
+  const drawRighttArrow = (g) => {
+    let t = 0.1;
+    const point0 = {
+      x: innerWidth - 80,
+      y: 70,
+    };
+
+    const controlPoint = {
+      x: innerWidth - 80,
+      y: 200,
+    };
+
+    const point1 = {
+      x: innerWidth - 200,
+      y: 200,
+    };
+
+    g.clear();
+    // control points
+    g.lineStyle(2, "#DF7868", 1);
+    let x0;
+    let y0;
+    let x1;
+    let y1;
+    let x;
+    let y;
+    g.moveTo(point0.x, point0.y);
+    for (let t = 0; t <= 1.0001; t += 0.02) {
+      x0 = point0.x + (controlPoint.x - point0.x) * t;
+      y0 = point0.y + (controlPoint.y - point0.y) * t;
+
+      x1 = controlPoint.x + (point1.x - controlPoint.x) * t;
+      y1 = controlPoint.y + (point1.y - controlPoint.y) * t;
+
+      x = x0 + (x1 - x0) * t;
+      y = y0 + (y1 - y0) * t;
+
+      g.lineTo(x, y);
+    }
+  };
+
   return (
     <Container x={0} height={0}>
       <Container>
@@ -59,6 +100,22 @@ const Arrows = () => {
           }
         />
         <Graphics draw={drawLeftArrow} />
+      </Container>
+
+      <Container>
+        <Text
+          position={{ x: innerWidth - 385, y: 190 }}
+          text="*Work in progress*"
+          style={
+            new PIXI.TextStyle({
+              fill: "#ffffff30",
+              wordWrapWidth: 400,
+              fontSize: 19,
+              fontFamily: "poppins",
+            })
+          }
+        />
+        <Graphics draw={drawRighttArrow} />
       </Container>
     </Container>
   );
